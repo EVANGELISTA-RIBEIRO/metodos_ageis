de314b99e8d738c7178e293d0338533dd0d558dd
Juan Guilherme <141837361+Gui-Muniz@users.noreply.github.com>
2024-04-01 20:44:15 -0300

    Update header.js

--- paginas/pagina_inicial/header.js
+++ paginas/pagina_inicial/header.js
@@ -1,12 +1,22 @@
 class Header extends HTMLElement {
 connectedCallback() {
-  this.innerHTML = '  <nav>
+  this.innerHTML = '   <header>
+        <img class="logo" src="/images/logo.png" alt="logo">
+        <nav>
             <ul class="nav_links">
                 <li><a href="#">Content</a></li>
                 <li><a href="#">Content</a></li>
                 <li><a href="#">Content</a></li>
             </ul>
-        </nav> ';
+        </nav>
+        <button class="button_acess">
+            <a href="#">
+                <p>
+                    <img class="icon_profile" src="/images/icon_profile.svg" alt="icon_profile"><center>Acessar</center>
+                </p>
+            </a>
+        </button>
+    </header> ';
   
            }
 }
